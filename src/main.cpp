29ec5a37e70cd70404a7561273d3eebeba6ba8d9
gstoneback <57008321+gstoneback@users.noreply.github.com>
2022-11-07 13:11:05 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,33 @@
-#include <argparse/argparse.h>
+#include <argparse/argparse.hpp>
 #include <iostream>
-
+#include <tesseract/baseapi.h>
+#include <string>
+#include <fstream>
+#include <cstdio>
 #define PROGRAM_NAME "pdr"
 
-int main() {
+int main(int argc, char* argv[]) {
   argparse::ArgumentParser args(PROGRAM_NAME);
   args.add_argument("infile").help("Input file");
+  args.add_argument("outfile").help("Output file");
   try {
     args.parse_args(argc, argv);
   }
   catch (const std::runtime_error& err) {
     std::cerr << err.what() << std::endl;
-    std::cerr << program;
+    std::cerr << args;
     std::exit(1);
   }
-  
+  std::ifstream infile;
+  infile.open(args.get<std::string>("infile"), std::ios::in | std::ios::binary);
+  if (!infile.is_open()) {
+    std::cerr << "No such file or directory: " << args.get<std::string>("infile") << std::endl;
+    std::exit(1);
+  }
+  char b;
+  while(infile.read(&b, 1)) {
+    printf("%.2x", b & 0xff);
+  }
+  std::cout << std::endl;
   return 0;
 }
